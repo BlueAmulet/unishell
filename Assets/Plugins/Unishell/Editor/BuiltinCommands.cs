dd1b5b7eabd2a5ab0d7cf1c7a64d62179dc31021
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2021-02-28 14:04:09 -0700

    Reformat code

--- Assets/Plugins/Unishell/Editor/BuiltinCommands.cs
+++ Assets/Plugins/Unishell/Editor/BuiltinCommands.cs
@@ -7,52 +7,49 @@ using System.IO;
 using System.Text;
 
 public class BuiltinCommands {
-	
+
 	public CommandEvaluator cmdEval;
-	
+
 	public BuiltinCommands(CommandEvaluator parent) {
 		cmdEval = parent;
 	}
-	
+
 	void ListVars() {
 		string vars = null;
 		try {
 			vars = cmdEval.CsharpEval.GetVars();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.ToString();
 		}
-		if(vars != null && vars.Length > 0) {
+		if (vars != null && vars.Length > 0) {
 			cmdEval.AppendOutput("Current vars:");
 			cmdEval.AppendOutput(vars);
 			cmdEval.AppendOutput("");
-		}
-		else {
+		} else {
 			cmdEval.AppendOutput("No current vars");
 		}
 	}
-	
+
 	void Clear() {
 		cmdEval.ClearConsole();
 	}
-	
+
 	void ListUsing() {
 		var val = cmdEval.CsharpEval.GetUsing();
-		if(val.Length > 0) {
+		if (val.Length > 0) {
 			cmdEval.AppendOutput("Current using:");
 			cmdEval.AppendOutput(val);
 			cmdEval.AppendOutput("");
-		}
-		else {
+		} else {
 			cmdEval.AppendOutput("No current using");
 			cmdEval.AppendOutput("");
 		}
 	}
-	
+
 	void ReloadScripts() {
 		cmdEval.LoadScripts();
 	}
-	
+
 	void Help() {
 		cmdEval.AppendOutput("Current shell commands:");
 		cmdEval.AppendOutput("\tlist vars: List all local variables in the shell");
@@ -66,9 +63,9 @@ public class BuiltinCommands {
 		cmdEval.AppendOutput("\tup arrow/down arrow: go through previous commands");
 		cmdEval.AppendOutput("");
 	}
-	
+
 	public bool CheckBuiltins(string toCheck) {
-		switch(toCheck) {
+		switch (toCheck) {
 		case "list vars":
 		case "list vars;":
 			ListVars();
@@ -93,4 +90,3 @@ public class BuiltinCommands {
 		return false;
 	}
 }
-
--- Assets/Plugins/Unishell/Editor/CommandEvaluator.cs
+++ Assets/Plugins/Unishell/Editor/CommandEvaluator.cs
@@ -14,59 +14,56 @@ public class CommandEvaluator {
 	public string consoleText = "";
 	public string commandText = "";
 	StringBuilder errInfo = new StringBuilder("");
-    ConsoleReportPrinter printer;
+	ConsoleReportPrinter printer;
 	public BuiltinCommands builtins;
 
-    public Evaluator CsharpEval;
-	
+	public Evaluator CsharpEval;
+
 	public CommandEvaluator() {
 		builtins = new BuiltinCommands(this);
 		//Evaluator.MessageOutput = new StringWriter(errInfo);
-        InitEval();
+		InitEval();
 	}
-	
+
 	public void ClearEval() {
 		//Evaluator.Init (new string[] {});
 	}
 
-    public void InitEval() {
-        var settings = new CompilerSettings();
-		foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            if(assembly == null || assembly.FullName.Contains("GeneratedCode") || assembly.FullName.Contains("eval-"))
-            {
-                continue;
-            }
-            Debug.Log($"Adding {assembly.FullName}");
-            settings.AssemblyReferences.Add(assembly.FullName);
-        }
-        printer = new ConsoleReportPrinter(new StringWriter(errInfo));
-        var context = new CompilerContext(settings, printer);
-        CsharpEval = new Evaluator(context);
-		CsharpEval.Run ("using UnityEngine; using System; using System.Collections.Generic;");
+	public void InitEval() {
+		var settings = new CompilerSettings();
+		foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+			if (assembly == null || assembly.FullName.Contains("GeneratedCode") || assembly.FullName.Contains("eval-")) {
+				continue;
+			}
+			Debug.Log($"Adding {assembly.FullName}");
+			settings.AssemblyReferences.Add(assembly.FullName);
+		}
+		printer = new ConsoleReportPrinter(new StringWriter(errInfo));
+		var context = new CompilerContext(settings, printer);
+		CsharpEval = new Evaluator(context);
+		CsharpEval.Run("using UnityEngine; using System; using System.Collections.Generic;");
 	}
 
-    public void LoadScripts() {
+	public void LoadScripts() {
 		List<string> scripts = ConsoleWindow.GetScriptContents();
 		foreach(var toParse in scripts) {
 			CsharpEval.Run(toParse);
 		}
 	}
-	
+
 	public bool AutocompleteBuffer() {
 		commandText = TryTabComplete(commandText);
 		return true;
 	}
-	
+
 	public bool UpHistory() {
-		if(previousCommands.Count > 0) {
-			if(commandScrollIdx == -1) {
+		if (previousCommands.Count > 0) {
+			if (commandScrollIdx == -1) {
 				commandScrollIdx = previousCommands.Count - 1;
 				return true;
-			}
-			else {
+			} else {
 				commandScrollIdx--;
-				if(commandScrollIdx < 0) {
+				if (commandScrollIdx < 0) {
 					commandScrollIdx = 0;
 				}
 			}
@@ -75,15 +72,14 @@ public class CommandEvaluator {
 		}
 		return false;
 	}
-	
-	public bool DownHistory() {		
-		if(commandScrollIdx != -1) {
+
+	public bool DownHistory() {
+		if (commandScrollIdx != -1) {
 			commandScrollIdx++;
-			if(commandScrollIdx < previousCommands.Count) {
+			if (commandScrollIdx < previousCommands.Count) {
 				commandText = previousCommands[commandScrollIdx];
 				return true;
-			}
-			else {
+			} else {
 				commandText = "";
 				commandScrollIdx = -1;
 				return true;
@@ -91,73 +87,70 @@ public class CommandEvaluator {
 		}
 		return false;
 	}
-	
+
 	public void Eval() {
 		RunCommand(commandText);
 		commandText = "";
 		commandScrollIdx = -1;
 	}
-	
+
 	string TryTabComplete(string cmdText) {
 		string prefix;
 		var results = CsharpEval.GetCompletions(cmdText, out prefix);
-		if(results != null && results.Length > 1) {
+		if (results != null && results.Length > 1) {
 			cmdText = cmdText + results[0];
 		}
 		return cmdText;
 	}
-	
+
 	void RunCommand(string commandText) {
-		if(commandText == null || commandText.Trim ().Equals ("")) {
+		if (commandText == null || commandText.Trim().Equals("")) {
 			return;
 		}
-		if(!builtins.CheckBuiltins(commandText)) {
-            printer.Reset();
+		if (!builtins.CheckBuiltins(commandText)) {
+			printer.Reset();
 			object obj;
 			bool result_set;
 			var command = commandText;
 			bool dots = false;
-			if(partialCommand != null) {
+			if (partialCommand != null) {
 				command = partialCommand + " " + commandText;
 				dots = true;
 			}
 			partialCommand = null;
 			string retval = CsharpEval.Evaluate(command, out obj, out result_set);
-			AppendOutput(string.Format ("{0} {1}", dots ? "... " : "> ", commandText));
-			if(retval == null) {
-				if(result_set) {
+			AppendOutput(string.Format("{0} {1}", dots ? "... " : "> ", commandText));
+			if (retval == null) {
+				if (result_set) {
 					AppendOutput(obj);
 				}
-				string toStore = command.Replace ("\n", " ");
+				string toStore = command.Replace("\n", " ");
 				AddCommandToBuffer(toStore);
-			}
-			else {
+			} else {
 				partialCommand = retval;
 			}
-            // MessageWriter.Flush();
-			if(errInfo.Length > 0) {
+			// MessageWriter.Flush();
+			if (errInfo.Length > 0) {
 				AppendOutput(errInfo.ToString());
 				errInfo.Remove(0, errInfo.Length);
 			}
-		}
-		else {
+		} else {
 			AddCommandToBuffer(commandText);
 		}
 	}
-	
+
 	void AddCommandToBuffer(string toAdd) {
 		previousCommands.Add(toAdd);
-		if(previousCommands.Count > MAX_CMD_BUFFER) {
-			previousCommands.RemoveAt (0);
+		if (previousCommands.Count > MAX_CMD_BUFFER) {
+			previousCommands.RemoveAt(0);
 		}
 	}
-	
+
 	public void AppendOutput(object toWrite) {
 		consoleText += toWrite.ToString() + "\n";
 	}
-	
+
 	public void ClearConsole() {
 		consoleText = "";
 	}
 }
-
